7de1b31f1fad64d4f76da34fed10fe6085563f37
Yuchen-1228 <73198253+Yuchen-1228@users.noreply.github.com>
2022-08-15 17:34:01 -0400

    Create Intake.java

---
+++ Group2/src/main/java/com/stuypulse/robot/subsystems/Intake.java
@@ -0,0 +1,28 @@
+package com.stuypulse.robot.subsystems;
+
+import static com.stuypulse.robot.constants.Ports.Intake.*;
+import static com.stuypulse.robot.constants.Motors.Intake.*;
+import static com.stuypulse.robot.constants.Settings.Intake.*;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.stuypulse.stuylib.control.Controller;
+import com.stuypulse.stuylib.network.SmartNumber;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+/** 
+
+*/
+
+public class Intake extends SubsystemBase {
+
+
+}
+
+
+
+
+
