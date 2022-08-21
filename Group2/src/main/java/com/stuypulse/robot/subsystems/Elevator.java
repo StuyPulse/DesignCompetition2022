c7c2a805b3a92043b4bed8c9f5e0834a51f0c722
yang zhou <independence106@gmail.com>
2022-08-21 13:27:58 -0400

    Created Elevator

--- Group2/src/main/java/com/stuypulse/robot/subsystems/Elevator.java
+++ Group2/src/main/java/com/stuypulse/robot/subsystems/Elevator.java
@@ -1,5 +1,50 @@
+package com.stuypulse.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+/**
+ * Methods: 
+ *  - Move Up,
+ *  - Move Down,
+ *  - Move set distance ()
+ *  - 
+ */
+
 public class Elevator extends SubsystemBase {
+
+    private final MotorControllerGroup motors;
+
+    private final 
+
+
+    public Elevator() {
+        motors = new MotorControllerGroup(new CANSparkMax(-1, MotorType.kBrushless), 
+                                          new CANSparkMax(-1, MotorType.kBrushless), 
+                                          new CANSparkMax(-1, MotorType.kBrushless), 
+                                          new CANSparkMax(-1, MotorType.kBrushless));
+        
+    }
+
+    private void move(double sped) {
+        motors.set(sped);
+    }
+
+    public void moveUp(double sped) {
+        move(+sped);
+    }
+
+    public void moveDown(double sped) {
+        move(-sped);
+    }
+
+    public double getDistance() {
+
+    }
+
+
 
 }
