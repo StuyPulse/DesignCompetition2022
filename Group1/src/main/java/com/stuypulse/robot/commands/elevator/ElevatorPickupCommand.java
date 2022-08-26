035447a1616741bb4d48f8074f54c49dbeeb5c8c
ambers7 <ambrshen@gmail.com>
2022-08-25 22:29:49 -0400

    Add Author Tags

--- Group1/src/main/java/com/stuypulse/robot/commands/elevator/ElevatorPickupCommand.java
+++ Group1/src/main/java/com/stuypulse/robot/commands/elevator/ElevatorPickupCommand.java
@@ -4,6 +4,10 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 
 import com.stuypulse.robot.subsystems.Elevator;
 
+/**
+  * @author Amber Shen
+ */
+ 
 public class ElevatorPickupCommand extends CommandBase {
     private final Elevator elevator;
     
