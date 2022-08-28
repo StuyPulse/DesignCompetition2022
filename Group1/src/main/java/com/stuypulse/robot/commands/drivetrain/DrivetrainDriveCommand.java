35138286f7c96808092f280d62ae0558b7f7615b
t1rbr <kelvinzhao29@gmail.com>
2022-08-27 23:25:19 -0400

    Fix DrivetrainDriveCommand

--- Group1/src/main/java/com/stuypulse/robot/commands/drivetrain/DrivetrainDriveCommand.java
+++ Group1/src/main/java/com/stuypulse/robot/commands/drivetrain/DrivetrainDriveCommand.java
@@ -5,39 +5,37 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 import com.stuypulse.robot.subsystems.Drivetrain;
 import com.stuypulse.stuylib.input.Gamepad;
 import com.stuypulse.stuylib.math.SLMath;
-import com.stuypulse.stuylib.streams.filters.IFilter;
+import com.stuypulse.stuylib.streams.IStream;
 import com.stuypulse.stuylib.streams.filters.LowPassFilter;
 import com.stuypulse.robot.constants.Settings;
 
 public class DrivetrainDriveCommand extends CommandBase {
     private final Drivetrain drivetrain;
-    private final Gamepad gamepad;
 
-    private final IFilter turnFilter;
-    private final IFilter driveFilter;
+    private final IStream turnFilter;
+    private final IStream driveFilter;
     
     public DrivetrainDriveCommand(Drivetrain drivetrain, Gamepad gamepad) {
         this.drivetrain = drivetrain;
-        this.gamepad = gamepad;
-
-        turnFilter = IFilter.create(
-            (x) -> SLMath.deadband(x, Settings.Drivetrain.TURN_DEADBAND.get()),
-            (x) -> SLMath.spow(x, Settings.Drivetrain.TURN_POWER.get()),
-            new LowPassFilter(Settings.Drivetrain.TURN_RC.get()),
-            (x) -> x * Settings.Drivetrain.MAX_TURN_SPEED.get()
-        );
-        driveFilter = IFilter.create(
-            (x) -> SLMath.deadband(x, Settings.Drivetrain.DRIVE_DEADBAND.get()),
-            (x) -> SLMath.spow(x, Settings.Drivetrain.DRIVE_POWER.get()),
-            new LowPassFilter(Settings.Drivetrain.DRIVE_RC.get()),
-            (x) -> x * Settings.Drivetrain.MAX_SPEED.get()
-        );
+        turnFilter = IStream.create(() -> gamepad.getLeftX()).
+            filtered(
+                x -> SLMath.deadband(x, Settings.Drivetrain.TURN_DEADBAND.get()),
+                x -> SLMath.spow(x, Settings.Drivetrain.TURN_POWER.get()),
+                new LowPassFilter(Settings.Drivetrain.TURN_RC.get()),
+                x -> x * Settings.Drivetrain.MAX_TURN_SPEED.get()
+            );
+        driveFilter = IStream.create(() -> gamepad.getRightTrigger() - gamepad.getLeftTrigger()).
+            filtered(
+                x -> SLMath.deadband(x, Settings.Drivetrain.DRIVE_DEADBAND.get()),
+                x -> SLMath.spow(x, Settings.Drivetrain.DRIVE_POWER.get()),
+                new LowPassFilter(Settings.Drivetrain.DRIVE_RC.get()),
+                x -> x * Settings.Drivetrain.MAX_SPEED.get()
+            );
         addRequirements(drivetrain);
     }
 
     @Override
     public void execute() {
-        drivetrain.arcadeDrive(driveFilter.get(gamepad.getLeftTrigger() - gamepad.getRightTrigger()), 
-                                turnFilter.get(gamepad.getLeftX()));
+        drivetrain.arcadeDrive(driveFilter.get(), turnFilter.get());
     }
 }
