561c1b6de42429ab9bca12271182aa43da974eaa
BenG49 <64862590+BenG49@users.noreply.github.com>
2022-08-30 17:30:29 -0400

    Move elevator commands into group2-elevator-commands

--- Group2/src/main/java/com/stuypulse/robot/commands/MoveElevatorDownCommand.java
+++
@@ -1,9 +0,0 @@
-package com.stuypulse.robot.commands;
-
-import com.stuypulse.robot.subsystems.Elevator;
-
-public class MoveElevatorDownCommand extends MoveElevatorCommand{
-    public MoveElevatorDownCommand(Elevator elevator) {
-        super(elevator, -1, false);
-    }
-}
--- Group2/src/main/java/com/stuypulse/robot/commands/MoveElevatorUpCommand.java
+++
@@ -1,9 +0,0 @@
-package com.stuypulse.robot.commands;
-
-import com.stuypulse.robot.subsystems.Elevator;
-
-public class MoveElevatorUpCommand extends MoveElevatorCommand{
-    public MoveElevatorUpCommand(Elevator elevator) {
-        super(elevator, -1, true);
-    }
-}
