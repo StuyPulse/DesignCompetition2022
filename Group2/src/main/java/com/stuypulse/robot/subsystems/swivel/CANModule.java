fb878535e2bb2007073863f918c5be224e0ec6d3
Oblivion409 <ben@goldfisher.com>
2022-08-31 14:47:13 -0400

    Create AbsoluteEncoder wrapper class and update CANModule

--- Group2/src/main/java/com/stuypulse/robot/subsystems/swivel/CANModule.java
+++ Group2/src/main/java/com/stuypulse/robot/subsystems/swivel/CANModule.java
@@ -5,20 +5,18 @@ import com.revrobotics.RelativeEncoder;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import com.stuypulse.robot.constants.Modules.ModuleConfig;
 import com.stuypulse.robot.constants.Settings.Swivel;
+import com.stuypulse.robot.util.AbsoluteEncoder;
 import com.stuypulse.robot.constants.Ports;
 
 import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.wpilibj.DutyCycleEncoder;
 
 public class CANModule extends SwivelModule {
     
     private final CANSparkMax turnMotor;
     private final CANSparkMax driveMotor;
 
-    private final DutyCycleEncoder turnEncoder;
+    private final AbsoluteEncoder turnEncoder;
     private final RelativeEncoder driveEncoder;
-
-    private final Rotation2d offset;
 
     public CANModule(ModuleConfig config) {
         super(config);
@@ -27,14 +25,12 @@ public class CANModule extends SwivelModule {
         turnMotor = new CANSparkMax(Ports.Swivel.TURN_MOTOR, MotorType.kBrushless);
 
         driveEncoder = driveMotor.getEncoder();
-        turnEncoder = new DutyCycleEncoder(Ports.Swivel.ABSOLUTE_ENCODER);
+        turnEncoder = new AbsoluteEncoder(Ports.Swivel.ABSOLUTE_ENCODER, config.zeroAngle);
 
         // by default the position is in rotations
         driveEncoder.setPositionConversionFactor(Swivel.WHEEL_CIRCUMFERENCE);
         // by defqault velocity is in rpm
         driveEncoder.setVelocityConversionFactor(Math.PI * Swivel.WHEEL_DIAMETER / 60.0);
-
-        offset = config.zeroAngle;
     }
 
 
@@ -50,11 +46,7 @@ public class CANModule extends SwivelModule {
 
     @Override
     protected Rotation2d getAngle() {
-        double radians = turnEncoder.getAbsolutePosition() % (2 * Math.PI);
-        if (radians > Math.PI)
-            radians -= 2 * Math.PI;
-    
-        return new Rotation2d(radians).minus(offset);
+        return turnEncoder.getAngle();
     }
 
     @Override
